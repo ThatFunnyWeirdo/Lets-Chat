1dceb0e2eeb171434ff89efc30ccdd39fb01d275
ThatFunnyWeirdo <112593486+ThatFunnyWeirdo@users.noreply.github.com>
2023-02-13 17:03:45 -0800

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBefeV0Nan4NbD3JMe3F5c0THjoYHX1D-k",
+    authDomain: "lets-chat-web-app-8463a.firebaseapp.com",
+    databaseURL: "https://lets-chat-web-app-8463a-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-web-app-8463a",
+    storageBucket: "lets-chat-web-app-8463a.appspot.com",
+    messagingSenderId: "71795894909",
+    appId: "1:71795894909:web:6c9562e3e24e91eefc1f14"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  
